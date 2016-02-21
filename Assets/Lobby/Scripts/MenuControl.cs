e4b67c192654fa69c10d79cca083c4a060cf7ef6
Wimok <n_wimok@kkumail.com>
2016-02-21 17:20:51 +0700

    Lobby test

---
+++ Assets/Lobby/Scripts/MenuControl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+//using System.Collections;
+using UnityEngine.Networking;
+
+public class MenuControl : MonoBehaviour
+{
+
+    public void StartLocalGame()
+    {
+        NetworkManager.singleton.StartHost();
+    }
+
+    public void JoinLocalGame()
+    {
+        if (hostNameInputIP.text != "Hostname")
+        {
+            NetworkManager.singleton.networkAddress = hostNameInputIP.text;
+        }
+        NetworkManager.singleton.StartClient();
+    }
+
+    public void StartMatchMaker()
+    {
+        NetworkManager.singleton.StartMatchMaker();
+    }
+
+    public UnityEngine.UI.Text hostNameInputIP;
+
+
+    void Start()
+    {
+        hostNameInputIP.text = NetworkManager.singleton.networkAddress;
+
+    }
+
+}
